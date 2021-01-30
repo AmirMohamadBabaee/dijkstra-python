82d8f7d9f60da1afd0e7159c910abba256a67124
Amir01 <iran.volleyball79@gmail.com>
2021-01-30 22:45:42 +0330

    Complete implementation of dijkstra algorithm

---
+++ Shortest Path/Dijkstra.py
@@ -0,0 +1,19 @@
+from Graph import Graph
+from MinHeap import MinHeap
+
+def dijkstra(graph: Graph, source: Graph.Node, destination: Graph.Node):
+    source.dist = 0
+    unexplored = MinHeap(array=graph.nodes)
+    explored = set()
+    while destination not in explored:
+        v = unexplored.get_minimum()
+        unexplored.delete_min()
+        explored.add(v)
+        neighbors = graph.get_vertex_neighbors(v)
+        if neighbors:
+            for neighbor in neighbors:
+                w, weight = neighbor
+                if v.dist + weight < w.dist:
+                    w.dist = v.dist + weight
+                    w.prev = v
+                    unexplored.modify(w, w)
