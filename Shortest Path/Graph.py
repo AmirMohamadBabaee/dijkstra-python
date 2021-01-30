ce27a273b33c62280405dfb5b2fcf8019e5c5f7c
Amir01 <iran.volleyball79@gmail.com>
2021-01-30 22:42:05 +0330

    fix bugs in func headers and property of Node class

--- Shortest Path/Graph.py
+++ Shortest Path/Graph.py
@@ -9,34 +9,45 @@ class Graph:
             self.id = id
             self.lat = lat
             self.long = long
-            self.dist = dist
-            self.prev = prev
+            self._dist = dist
+            self._prev = prev
+
+        @property
+        def dist(self):
+            return self._dist
+
+        @property
+        def prev(self):
+            return self._prev
 
         @dist.setter
         def dist(self, value):
-            self.dist = value
+            self._dist = value
 
         @prev.setter
         def prev(self, value):
-            self.prev = value
+            self._prev = value
 
-        def __lt__(self, obj: self.__class__):
+        def __lt__(self, obj):
             return self.dist < obj.dist
 
-        def __repr__(self):
-                return '(' + str(self.name) + ', ' + str(self.dist) + ', ' + str(self.prev) + ')'
+        def __int__(self):
+            return self.id
 
-    def __init__(self, nodes: set):
+        def __repr__(self):
+                return '(' + str(self.id) + ', ' + str(self.dist) + ', ' + str(self.prev) + ')'
+
+    def __init__(self, nodes= list()):
         self.nodes = nodes
         self.adjacency_list = defaultdict(list)
 
-    def add_vertex(self, vertex: self.Node):
-        self.nodes.add(vertex)
+    def add_vertex(self, vertex_id, vertex_lat, vertex_long):
+        self.nodes.append(self.Node(vertex_id, vertex_lat, vertex_long))
 
-    def add_edge(self, v1: self.Node, v2: self.Node, weight: int):
+    def add_edge(self, v1, v2, weight: int):
         self.adjacency_list[v1.id].append((v2, weight))
         self.adjacency_list[v2.id].append((v1, weight))
 
-    def get_vertex_neighbors(self, vertex: self.Node):
+    def get_vertex_neighbors(self, vertex):
         if vertex in self.nodes:
             return self.adjacency_list[vertex.id]
